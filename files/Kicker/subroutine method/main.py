a997648b63298a6f3fc600476378d9c2e835ccae
Lukashab21 <lih21@go4more.de>
2024-04-26 16:07:25 +0200

    new "notify" function, ideas overview

--- files/Kicker/subroutine method/main.py
+++ files/Kicker/subroutine method/main.py
@@ -1,9 +1,15 @@
 """
 THIS FILE IS NOT ACTUALLY EXECUTABLE YET
-"import main_modules.py as a", more useful name can be chosen later (Ctrl+F replace); "import ... as ..." is needed for variables to actually be updated while running, simple "import ..." doesnt do it
+"import main_modules.py as a", more useful name can be chosen later (Ctrl+F replace); "import ... as ..." is needed for variables to actually be updated while running, "from ... import ..." doesnt do it
+
+to be added:
+- replace "while True" with bool for emergency shutdown
+- use try/except/finally
+- use threads library for polling
+
 """
 __author__ = "Lukas Haberkorn"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __status__ = "heavy WIP"
 
 
@@ -22,7 +28,7 @@ a.display_screensaver()
 while True:
     # check start button
     if a.poll_startbutton() is True:
-        a.notify_gamestart()
+        a.notify_auvares("gamestart")
         a.set_status_running()
         a.display_score()
     a.wait_after_loop()
@@ -31,23 +37,23 @@ while True:
         # check for goals
         if a.poll_sensor1 == True:
             a.score_addgoal1()
-            a.notify_goal()
+            a.notify_auvares("goal")
             a.display_goal()
             a.wait_after_goal()
             a.display_score()
         if a.poll_sensor2 == True:
             a.score_addgoal2()
-            a.notify_goal()
+            a.notify_auvares("goal")
             a.display_goal()
             a.wait_after_goal()
             a.display_score()
         # check for Kurbelvergehen (maybe has to be called more often)
         if a.poll_foul == True:
-            a.notify_foul()
+            a.notify_auvares("foul")
         # check win condition
         if (a.goals1 == 6 or a.goals2 == 6) or (a.goals1 == 5 and a.goals2 == 5):
             a.set_status_ready()
-            a.notify_gameover()
+            a.notify_auvares("gameover")
             a.display_gameover()
             a.database_savegame()
             a.wait_after_game()
