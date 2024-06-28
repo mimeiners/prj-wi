bd151240f6e886d641cb0ce6c11e2db0413f7e14
davidwittig <davidwittig@tutanota.de>
2024-06-28 19:00:28 +0200

    minor update

--- files/Kicker/html/register.php
+++ files/Kicker/html/register.php
@@ -10,30 +10,50 @@
 require_once "config.php";
  
 // Variablen für die Eingabefelder initialisieren
-$username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = "";
- 
+$username = $password = "";
+$username_error = $password_error = "";
+$password_check = $password_check_error = "";
+
 // Eingabefelderinhalt prüfen und ggf. verarbeiten
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    // Passworteingabe prüfen
+    if(empty(trim($_POST["password"]))){
+        $password_error = "Bitte ein Passwort eingeben.";     
+    } elseif(strlen(trim($_POST["password"])) < 5) {
+        $password_error = "Das Passwort sollte mindestens fünf Zeichen lang sein.";
+    } else {
+        $password = trim($_POST["password"]);
+    }
+    
+    // Passworteingabewiederholung prüfen
+    if(empty(trim($_POST["confirm_password"]))){
+        $password_check_error = "Bitte das Passwort wiederholen.";     
+    } else {
+        $password_check = trim($_POST["confirm_password"]);
+        if(empty($password_error) && ($password != $password_check)){
+            $password_check_error = "Passwort stimmt nicht überein.";
+        }
+    }
  
     // Nutzernameneingabe prüfen
     if(empty(trim($_POST["username"]))){
-        $username_err = "Bitte Nutzernamen angeben.";
+        $username_error = "Bitte Nutzernamen angeben.";
     } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
-        $username_err = "Dieser Nutzername enthält unzulässige Zeichen.";
-    } else{
+        $username_error = "Dieser Nutzername enthält unzulässige Zeichen.";
+    } else {
         // Nutzernameneingabe für Datenbankeintragung vorbereiten
-        $sql = "SELECT id FROM users WHERE username = ?";
+        $sql_cmd = "SELECT id FROM users WHERE username = ?";
         
-        if($stmt = mysqli_prepare($link, $sql)){
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-            $param_username = trim($_POST["username"]);
+        if($stmt = mysqli_prepare($link, $sql_cmd)){
+            mysqli_stmt_bind_param($stmt, "s", $username_parameter);
+            $username_parameter = trim($_POST["username"]);
 
             if(mysqli_stmt_execute($stmt)){
                 mysqli_stmt_store_result($stmt);
                 
                 if(mysqli_stmt_num_rows($stmt) == 1){
-                    $username_err = "Dieser Nutzer existiert bereits.";
+                    $username_error = "Dieser Nutzer existiert bereits.";
                 } else{
                     $username = trim($_POST["username"]);
                 }
@@ -44,43 +64,23 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             mysqli_stmt_close($stmt);
         }
     }
-    
-    // Passworteingabe prüfen
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Bitte ein Passwort eingeben.";     
-    } elseif(strlen(trim($_POST["password"])) < 5){
-        $password_err = "Das Passwort sollte mindestens fünf Zeichen lang sein.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-    
-    // Passworteingabewiederholung prüfen
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Bitte das Passwort wiederholen.";     
-    } else{
-        $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($password_err) && ($password != $confirm_password)){
-            $confirm_password_err = "Passwort stimmt nicht überein.";
-        }
-    }
-    
+      
     // Datenbankeintragung durchführen, wenn Nutzereingaben zulässig sind
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+    if(empty($username_error) && empty($password_error) && empty($password_check_error)){
         
         // Nutzername und Passwort für Dateinbankeintrag vorbereiten
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $sql_cmd = "INSERT INTO users (username, password) VALUES (?, ?)";
          
-        if($stmt = mysqli_prepare($link, $sql)){
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
-
-            $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT);
+        if($stmt = mysqli_prepare($link, $sql_cmd)){
+            mysqli_stmt_bind_param($stmt, "ss", $username_parameter, $password_parameter);
+            $username_parameter = $username;
+            $password_parameter = password_hash($password, PASSWORD_DEFAULT);
             
             // Datenbankeintragung durchführen
             if(mysqli_stmt_execute($stmt)){
                 // Umleitung nach erfolgreicher Registrierung
                 header("location: login.php");
-            } else{
+            } else {
                 echo "Unbekannte Fehlermeldung";
             }
 
@@ -115,18 +115,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                             <div class="form-group">
                                 <label for="username">Nutzername</label>
-                                <input type="text" id="username" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
-                                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+                                <input type="text" id="username" name="username" class="form-control <?php echo (!empty($username_error)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                                <span class="invalid-feedback"><?php echo $username_error; ?></span>
                             </div>
                             <div class="form-group">
                                 <label for="password">Passwort</label>
-                                <input type="password" id="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
-                                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+                                <input type="password" id="password" name="password" class="form-control <?php echo (!empty($password_error)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                                <span class="invalid-feedback"><?php echo $password_error; ?></span>
                             </div>
                             <div class="form-group">
                                 <label for="confirm_password">Passwort wiederholen</label>
-                                <input type="password" id="confirm_password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
-                                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+                                <input type="password" id="confirm_password" name="confirm_password" class="form-control <?php echo (!empty($password_check_error)) ? 'is-invalid' : ''; ?>" value="<?php echo $password_check; ?>">
+                                <span class="invalid-feedback"><?php echo $password_check_error; ?></span>
                             </div>
                             <div class="form-group">
                                 <input type="submit" class="btn btn-primary" value="Submit">
